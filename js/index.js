ddd6d5f4b650aeb371ea35a3399286c618e93ee4
chris <cseiter@gmail.com>
2021-04-18 08:50:08 -0400

    fetch data available outside function

--- js/index.js
+++ js/index.js
@@ -2,9 +2,8 @@ const rootURL = "http://localhost:3000/";
 
 function getMonsters() {
     const builtURL = `${rootURL}monsters/?_limit=50&_page=1`;
-    fetch(builtURL)
+    return fetch(builtURL)
         .then(r => r.json())
-        .then (data => console.log(data));
     }
 
-getMonsters();
+getMonsters().then(console.log);
